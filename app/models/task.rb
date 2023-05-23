7694de58780c01fe513e0e0971873fbf5eb129a8
simon hu <simon.hu.1999@gmail.com>
2023-05-23 23:10:11 +0200

    Added refactoring

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,2 @@
 class Task < ApplicationRecord
-  after_initialize :init
-
-  def init
-    self.completed = false
-  end
 end
---
+++ db/migrate/20230523170419_change_tasks_completed.rb
@@ -0,0 +1,5 @@
+class ChangeTasksCompleted < ActiveRecord::Migration[7.0]
+  def change
+    change_column :tasks, :completed, :boolean, :default => false
+  end
+end
